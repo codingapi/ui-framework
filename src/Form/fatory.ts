2aaba44147bd2183eb8bf68a6ce9f9f594219a2e
lorne <1991wangliang@gmail.com>
2025-05-14 20:59:03 +0800

    fix #1

--- src/Form/fatory.ts
+++ src/Form/fatory.ts
@@ -1,8 +1,8 @@
 import React from "react";
-import {FormItemProps} from "./types";
+import {FormField, FormItemProps} from "./types";
 
 export class FormFactory {
-    private readonly items = new Map<string, React.Component<FormItemProps>>;
+    private readonly items = new Map<string, React.ComponentType<FormItemProps>>;
 
     private constructor() {
 
@@ -14,12 +14,26 @@ export class FormFactory {
         return this.instance;
     }
 
-    public addItem(type: string, item: React.Component<FormItemProps>): void {
+    public addItem(type: string, item: React.ComponentType<FormItemProps>): void {
         this.items.set(type, item);
     }
 
-    public getItem(type: string): React.Component<FormItemProps> | undefined {
+    public getItem(type: string): React.ComponentType<FormItemProps> | undefined {
         return this.items.get(type);
+    }
+
+    public create(filed:FormField){
+        const type = filed.type;
+        const props = filed.props;
+
+        const item = this.getItem(type);
+        if (item) {
+            return React.createElement(item, {
+                ...props,
+                key: props.name as string
+            });
+        }
+        return null;
     }
 
 }
